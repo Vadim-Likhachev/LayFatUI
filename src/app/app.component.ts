d85dbf90493908abac1e524ce5690b4b52922482
Vadim <vadlen2149@gmail.com>
2023-03-22 15:31:38 +0300

    init LayFatUI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  host: {class: 'app'}
 })
 export class AppComponent {
-  title = 'LayFatUI';
+  title = 'LayFat';
 }
